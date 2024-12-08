6d0aad216015322e197539818f2d74c3d228668a
James Sandy <jamessandy95@gmail.com>
2024-12-08 10:36:29 +0000

    part 1 refactor

--- days/day08/main.go
+++ days/day08/main.go
@@ -16,28 +16,34 @@ func Solve1(input_lines string) int {
 				if i <= j {
 					continue
 				}
-				x_diff := coords[i][0] - coords[j][0]
-				y_diff := coords[i][1] - coords[j][1]
-				antinode_1 := [2]int{coords[i][0] + x_diff, coords[i][1] + y_diff}
-				antinode_2 := [2]int{coords[i][0] - 2*x_diff, coords[i][1] - 2*y_diff}
+				antinodes := get_antinodes_1(coords[i], coords[j], x_max, y_max)
 
-				if coord_on_map(antinode_1, x_max, y_max) {
-					_, exists := uniqute_antinodes[antinode_1]
-					if !exists {
-						uniqute_antinodes[antinode_1] = struct{}{}
+				for _, antinode := range antinodes {
+					if coord_on_map(antinode, x_max, y_max) {
+						_, exists := uniqute_antinodes[antinode]
+						if !exists {
+							uniqute_antinodes[antinode] = struct{}{}
+						}
 					}
 				}
-				if coord_on_map(antinode_2, x_max, y_max) {
-					_, exists := uniqute_antinodes[antinode_2]
-					if !exists {
-						uniqute_antinodes[antinode_2] = struct{}{}
-					}
-				}
+
 			}
 		}
 	}
 
 	return len(uniqute_antinodes)
+}
+
+func get_antinodes_1(a [2]int, b [2]int, x_max int, y_max int) [][2]int {
+	antinodes := [][2]int{}
+	x_diff := a[0] - b[0]
+	y_diff := a[1] - b[1]
+	antinode_1 := [2]int{a[0] + x_diff, a[1] + y_diff}
+	antinodes = append(antinodes, antinode_1)
+	antinode_2 := [2]int{a[0] - 2*x_diff, a[1] - 2*y_diff}
+	antinodes = append(antinodes, antinode_2)
+
+	return antinodes
 }
 
 func coord_on_map(coord [2]int, x_max int, y_max int) bool {
@@ -55,8 +61,8 @@ func abs(a int, b int) int {
 		return b - a
 	}
 }
-func parse_input(input_lines string) (map[rune][][]int, int, int) {
-	antennas := make(map[rune][][]int)
+func parse_input(input_lines string) (map[rune][][2]int, int, int) {
+	antennas := make(map[rune][][2]int)
 
 	lines := strings.Split(input_lines, "\n")
 
@@ -66,13 +72,13 @@ func parse_input(input_lines string) (map[rune][][]int, int, int) {
 				continue
 			}
 
-			coord := []int{x, y}
+			coord := [2]int{x, y}
 			coords, exists := antennas[antenna]
 
 			if exists {
 				antennas[antenna] = append(coords, coord)
 			} else {
-				antennas[antenna] = [][]int{coord}
+				antennas[antenna] = [][2]int{coord}
 			}
 		}
 	}
