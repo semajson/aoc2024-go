bad098dce8fb94994b02466ceda5fb4c5f02a61c
James Sandy <jamessandy95@gmail.com>
2024-12-22 22:16:22 +0000

    day22 fail

--- days/day22/main.go
+++ days/day22/main.go
@@ -23,10 +23,70 @@ func Solve1(input_lines string) int {
 func Solve2(input_lines string) int {
 	nums := parse_input(input_lines)
 
-	// Calc difference when sorted
-	println(len(nums))
+	max_total := 0
+	for i := -9; i <= 9; i++ {
+		for j := -9; j <= 9; j++ {
+			for k := -9; k <= 9; k++ {
+				for l := -9; l <= 9; l++ {
+					seq := [4]int{i, j, k, l}
 
-	return 2
+					if seq[0] == -2 && seq[1] == 1 && seq[2] == -1 && seq[3] == 3 {
+						println("test")
+
+					}
+
+					if seq[0] == -9 && seq[1] == 9 && seq[2] == -1 && seq[3] == 0 {
+						println("test")
+
+					}
+
+					total := 0
+					for _, num := range nums {
+						num_bananas := Get_num_bananas(num, seq)
+						total += num_bananas
+					}
+					if total > max_total {
+						max_total = total
+					}
+					if total == 24 {
+						println("error")
+					}
+				}
+			}
+		}
+	}
+
+	return max_total
+}
+
+func Get_num_bananas(secret int, seq [4]int) int {
+	// Todo, make diffs more efficient
+	diffs := [4]int{99, 99, 99, 99}
+	curr := secret
+	for i := 0; i < 2000; i++ {
+		next := get_next_secret(curr)
+		diff := (next%10 - curr%10) % 10
+
+		diffs[i%4] = diff
+
+		if i >= 4 {
+			match := true
+			for j := range seq {
+				if seq[j] != diffs[((i%4)+j+1)%4] {
+					match = false
+					break
+				}
+
+			}
+			if match {
+				println("test")
+				return next % 10
+			}
+		}
+
+		curr = next
+	}
+	return 0
 }
 
 func get_next_secret(curr_secret int) int {
--- days/day22/main_test.go
+++ days/day22/main_test.go
@@ -17,13 +17,101 @@ func TestDayExampleP1(t *testing.T) {
 
 func TestDayExampleP2(t *testing.T) {
 	example := `1
-10
-100
+2
+3
 2024`
 	result := Solve2(example)
-	expect := 31
+	expect := 23
 
 	if result != expect {
 		t.Errorf("Failed! \nSolve(%q)\nEquals: %d; \nWanted: %d", example, result, expect)
+	}
+}
+
+func TestDayExample3P2(t *testing.T) {
+
+	result := Get_num_bananas(123, [4]int{-1, -1, 0, 2})
+	expect := 6
+
+	if result != expect {
+		t.Errorf("Failed!\nEquals: %d; \nWanted: %d", result, expect)
+	}
+}
+
+func TestDayExample4P2(t *testing.T) {
+
+	result := Get_num_bananas(1, [4]int{-2, 1, -1, 3})
+	expect := 7
+
+	if result != expect {
+		t.Errorf("Failed!\nEquals: %d; \nWanted: %d", result, expect)
+	}
+}
+func TestDayExample5P2(t *testing.T) {
+
+	result := Get_num_bananas(2, [4]int{-2, 1, -1, 3})
+	expect := 7
+
+	if result != expect {
+		t.Errorf("Failed!\nEquals: %d; \nWanted: %d", result, expect)
+	}
+}
+
+func TestDayExample6P2(t *testing.T) {
+
+	result := Get_num_bananas(3, [4]int{-2, 1, -1, 3})
+	expect := 0
+
+	if result != expect {
+		t.Errorf("Failed!\nEquals: %d; \nWanted: %d", result, expect)
+	}
+}
+
+func TestDayExample7P2(t *testing.T) {
+
+	result := Get_num_bananas(2024, [4]int{-2, 1, -1, 3})
+	expect := 9
+
+	if result != expect {
+		t.Errorf("Failed!\nEquals: %d; \nWanted: %d", result, expect)
+	}
+}
+
+func TestDayExample10P2(t *testing.T) {
+
+	result := Get_num_bananas(1, [4]int{-9, 9, -1, 0})
+	expect := 7
+
+	if result != expect {
+		t.Errorf("Failed!\nEquals: %d; \nWanted: %d", result, expect)
+	}
+}
+func TestDayExample11P2(t *testing.T) {
+
+	result := Get_num_bananas(2, [4]int{-9, 9, -1, 0})
+	expect := 7
+
+	if result != expect {
+		t.Errorf("Failed!\nEquals: %d; \nWanted: %d", result, expect)
+	}
+}
+
+func TestDayExample12P2(t *testing.T) {
+
+	result := Get_num_bananas(3, [4]int{-9, 9, -1, 0})
+	expect := 0
+
+	if result != expect {
+		t.Errorf("Failed!\nEquals: %d; \nWanted: %d", result, expect)
+	}
+}
+
+func TestDayExample13P2(t *testing.T) {
+
+	result := Get_num_bananas(2024, [4]int{-9, 9, -1, 0})
+	expect := 9
+
+	if result != expect {
+		t.Errorf("Failed!\nEquals: %d; \nWanted: %d", result, expect)
 	}
 }
